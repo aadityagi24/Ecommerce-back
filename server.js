f49211f58a84c9cca6c518aace4d676bea3d191b
Aadi Tyagi <aadityagi1232@gmail.com>
2025-07-08 17:59:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db'); 
 const authRoute = require('./routes/authRoute.js');
 const categoryRoutes = require('./routes/categoryRoutes.js');
-const productRoutes = require('./routes/productRoutes')
+const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
-
 
 // config env
 dotenv.config();
@@ -16,25 +15,31 @@ connectDB();
 // rest object
 const app = express();
 
-// middleware to parse JSON
-app.use(express.json()); // for reading req.body in POST requests
-app.use(cors());
+// CORS Configuration
+app.use(cors({
+  origin: "https://ecommerce-front-three-sigma.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
-// routes
+// Middleware to parse JSON
+app.use(express.json()); 
+
+// Routes
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
-
-// rest api
+// Default route
 app.get('/', (req, res) => {
   res.send("Welcome to my new website");
 });
 
-// port
+// Port
 const PORT = process.env.PORT || 8080;
 
-// run listen
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
