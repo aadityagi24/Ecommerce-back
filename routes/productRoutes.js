4880f2a2dbc31d196abaafdf93af24004b8efe64
Aadi Tyagi <aadityagi1232@gmail.com>
2025-07-15 22:31:50 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+const formidable = require('express-formidable');
 const {
-   createProductController,
+  createProductController,
   deleteProductController,
   getProductController,
   getSingleProductController,
@@ -10,75 +10,66 @@ const {
   productListController,
   productPhotoController,
   updateProductController,
-   searchProductController,
-   realtedProductController,
-   productCategoryController,
-   braintreeTokenController,
-    brainTreePaymentController,
-
-} = require("../controllers/productController.js");
-
-const { isAdmin, requireSignIn } =require( "../middlewares/authMiddleware.js");
-const formidable = require( "express-formidable"); ///to easly upload phtots
+  searchProductController,
+  realtedProductController,
+  productCategoryController,
+  braintreeTokenController,
+  brainTreePaymentController,
+} = require('../controllers/productController.js');
+const { isAdmin, requireSignIn } = require('../middlewares/authMiddleware.js');
 
 const router = express.Router();
 
-//routes
+// Create product
 router.post(
-  "/create-product",
+  '/create-product',
   requireSignIn,
   isAdmin,
   formidable(),
   createProductController
 );
-//routes
+
+// Update product
 router.put(
-  "/update-product/:pid",
+  '/update-product/:pid',
   requireSignIn,
   isAdmin,
   formidable(),
   updateProductController
 );
 
-//get products
-router.get("/get-product", getProductController);
+// Get all products
+router.get('/get-product', getProductController);
 
+// Get single product
+router.get('/get-product/:slug', getSingleProductController);
 
-//single product
-router.get("/get-product/:slug", getSingleProductController);
+// Get product photo
+router.get('/product-photo/:pid', productPhotoController);
 
-//get photo
-router.get("/product-photo/:pid", productPhotoController);
+// Delete product
+router.delete('/delete-product/:pid', requireSignIn, isAdmin, deleteProductController);
 
-//delete rproduct
-router.delete("/delete-product/:pid", deleteProductController);
+// Filter products
+router.post('/product-filters', productFiltersController);
 
-//filter product
-router.post("/product-filters", productFiltersController);
+// Product count
+router.get('/product-count', productCountController);
 
-//product count
-router.get("/product-count", productCountController);
+// Product per page
+router.get('/product-list/:page', productListController);
 
-//product per page
-router.get("/product-list/:page", productListController);
+// Search product
+router.get('/search/:keyword', searchProductController);
 
-//search product
-router.get("/search/:keyword", searchProductController);
+// Related products
+router.get('/related-product/:pid/:cid', realtedProductController);
 
-//similar product
-router.get("/related-product/:pid/:cid", realtedProductController);
+// Category wise product
+router.get('/product-category/:slug', productCategoryController);
 
-//category wise product
-router.get("/product-category/:slug", productCategoryController);
-
-
-//payments routes
-
-//token
-router.get("/braintree/token", braintreeTokenController);
-
-//payments
-router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
-
+// Braintree payment routes
+router.get('/braintree/token', braintreeTokenController);
+router.post('/braintree/payment', requireSignIn, brainTreePaymentController);
 
 module.exports = router;
