621ff35d7cddf289c18d08c834bc4d712c95a663
Aadi Tyagi <aadityagi1232@gmail.com>
2025-07-21 21:28:45 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectDB =async()=>{
-    try {
-        const conn =await mongoose.connect(process.env.Mongo_URL)
-        console.log(`conected to database ${conn.connection.host}`);
-    } catch (error) {
-        
-        console.log(`error${error}`)
-
-
-
-    }
-}
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL); 
+    console.log(` Connected to database: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(` Database connection error: ${error.message}`);
+    process.exit(1); // Optional: exit on failure
+  }
+};
 
 module.exports = connectDB;
